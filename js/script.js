7a25387ba5c332705ec56fe859a7f78a2204b4b8
Filipe Santos <outerspaceguy@live.co.uk>
2022-09-30 23:09:44 -0300

    added some responsivity

--- js/script.js
+++
@@ -1,7 +0,0 @@
-var menu = document.querySelector(".menu")
-var navMenu = document.querySelector(".navigation ul")
-
-menu.addEventListener("click",() => {
-    menu.classList.toggle("ativo")
-    navMenu.classList.toggle("ativo")
-})
